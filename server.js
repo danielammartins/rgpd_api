33c7869d11c1886f6500cbd2997f0725bbf4ff55
danielammartins <martins.danielamadeira@gmail.com>
2022-06-24 21:01:43 +0100

    finished GET and PUT endpoints for retrieving content from editor

--- server.js
+++ server.js
@@ -13,27 +13,41 @@ const port = 3011
 
 app.use(cors({
   origin: ["http://localhost:3000"],
-  methods: ["GET", "POST", "DELETE"],
+  methods: ["GET", "PUT", "DELETE"],
   credentials: true,
-})
-);
+}));
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 db.connect(function(err) {
   if(err) throw err;
   console.log("Connected to MySQL database!");
 });
 
+app.get('/tecontent/:id', (req, res) => {
+  db.query(`SELECT * FROM tecontent WHERE id = ${db.escape(req.params.id)}`, function(err, data, fields) {
+    if (err) throw err;
+    res.json(data);
+  });
+});
+
+app.put('/tecontent/:id', (req, res) => {
+  let values = [
+    req.params.id,
+    req.body.content
+  ];
+  db.query(`REPLACE INTO tecontent(id, content) VALUES (?)`, [values], function(err, data, fields) {
+    if (err) throw err;
+    res.json({
+      status: 200,
+      message: "Conteúdo atualizado com sucesso!"
+    })
+  })
+});
+
 app.listen(port, () => {
   console.log(`rgpd-api listening at http://localhost:${port}`)
 });
 
-app.get('/api/text', (req, res) => {
-  db.query("SELECT * FROM tecontent WHERE id='1'", function(err, data, fields) {
-    if (err) throw err;
-    res.json(data[0].content);
-  });
-});
 
-app.get('/text', (req, res) => {
-  console.log("Oi2");   
-});
